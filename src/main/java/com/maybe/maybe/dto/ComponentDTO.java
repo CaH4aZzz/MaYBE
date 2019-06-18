161a7715a6bdf54a7f29ac7520d4bc2fe4e300e2
kosten27 <kosten27@gmail.com>
2019-06-18 20:36:15 +0300

    ComponentDTO java feature: ComponentDTO.java: Added DTO for the entity Component

---
+++ src/main/java/com/maybe/maybe/dto/ComponentDTO.java
@@ -0,0 +1,55 @@
+package com.maybe.maybe.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ComponentDTO implements Serializable {
+
+    private Long id;
+    private String name;
+    private Integer measureId;
+    private BigDecimal quantity;
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMeasureId() {
+        return measureId;
+    }
+
+    public void setMeasureId(Integer measureId) {
+        this.measureId = measureId;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
