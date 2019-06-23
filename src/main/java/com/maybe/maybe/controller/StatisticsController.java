b9aba9f89fac92042b37b7dc295842b1d847a404
CaH4aZzz <CaH4aZzz@ua.fm>
2019-06-24 01:02:16 +0300

    DTO, controller, service with test method added

--- src/main/java/com/maybe/maybe/controller/StatisticsController.java
+++ src/main/java/com/maybe/maybe/controller/StatisticsController.java
@@ -1,24 +1,34 @@
 package com.maybe.maybe.controller;
 
+import com.maybe.maybe.dto.ComponentReportDTO;
 import com.maybe.maybe.dto.SummaryDTO;
+import com.maybe.maybe.entity.Invoice;
+import com.maybe.maybe.repository.InvoiceRepository;
 import com.maybe.maybe.service.StatisticsService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/statistics")
 public class StatisticsController {
 
     private StatisticsService statisticsService;
+    private InvoiceRepository invoiceRepository;
 
-    public StatisticsController(StatisticsService statisticsService) {
+    public StatisticsController(StatisticsService statisticsService, InvoiceRepository invoiceRepository) {
         this.statisticsService = statisticsService;
+        this.invoiceRepository = invoiceRepository;
     }
 
     @GetMapping("/summary")
@@ -27,5 +37,15 @@ public class StatisticsController {
             @RequestParam(required = false)  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTo
     ) {
         return statisticsService.getSummaryReport(dateAt, dateTo);
+    }
+
+    @GetMapping("/component-report")
+    public Page<Invoice> getComponentReportDTO(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFrom,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTo,
+            @PageableDefault(size=Integer.MAX_VALUE) Pageable pageable
+    ){
+
+        return statisticsService.getComponentReport(dateFrom, dateTo, pageable);
     }
 }
--- src/main/java/com/maybe/maybe/service/StatisticsService.java
+++ src/main/java/com/maybe/maybe/service/StatisticsService.java
@@ -1,11 +1,17 @@
 package com.maybe.maybe.service;
 
+import com.maybe.maybe.dto.ComponentReportDTO;
 import com.maybe.maybe.dto.SummaryDTO;
+import com.maybe.maybe.entity.Invoice;
+import com.maybe.maybe.repository.InvoiceRepository;
 import com.maybe.maybe.repository.OrderRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -18,9 +24,11 @@ public class StatisticsService {
     private static final Logger LOG = LoggerFactory.getLogger(StatisticsService.class);
 
     private OrderRepository orderRepository;
+    private InvoiceRepository invoiceRepository;
 
-    public StatisticsService(OrderRepository orderRepository) {
+    public StatisticsService(OrderRepository orderRepository, InvoiceRepository invoiceRepository) {
         this.orderRepository = orderRepository;
+        this.invoiceRepository = invoiceRepository;
     }
 
     public List<SummaryDTO> getSummaryReport(LocalDate dateAt, LocalDate dateTo) {
@@ -30,5 +38,20 @@ public class StatisticsService {
             dateTo = LocalDate.now();
         }
         return orderRepository.getSummaryReport(LocalDateTime.of(dateAt, LocalTime.MIDNIGHT), LocalDateTime.of(dateTo, LocalTime.MIDNIGHT));
+    }
+
+    public Page<Invoice> getComponentReport(LocalDate dateFrom, LocalDate dateTo, Pageable pageable){
+        if (Objects.isNull(dateFrom)){
+            Long firstId = 1L;
+            Invoice invoice = invoiceRepository.findById(firstId)
+                    .orElseThrow(() -> new EntityNotFoundException("There is no data yet"));
+            dateFrom = invoice.getDateCreated().toLocalDate();
+        }
+
+        if(Objects.isNull(dateTo)){
+            dateTo = LocalDate.now();
+        }
+
+        return invoiceRepository.findAllByDateCreatedIsBetween(dateFrom.atStartOfDay(), dateTo.atStartOfDay(), pageable);
     }
 }
