1ffbd4c7710389e0bd9cbfbd85c53fbb95826f84
klitsenko.dmitriy@gmail.com <klitsenko.dmitriy@gmail.com>
2019-06-25 12:56:32 +0300

    Entity desk refactor: entity\Desk.java: change table name to 'desk'

--- src/main/java/com/maybe/maybe/entity/Desk.java
+++ src/main/java/com/maybe/maybe/entity/Desk.java
@@ -1,12 +1,10 @@
 package com.maybe.maybe.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 
 @Entity
-@Table(name = "customer")
+@Table(name = "desk")
 public class Desk extends AbstractNameEntity {
 }
