3d568a1f32d33f3851c635fc77a854e3d020eb7f
klitsenko.dmitriy@gmail.com <klitsenko.dmitriy@gmail.com>
2019-06-30 20:21:00 +0300

    Merge branch 'dev' of C:\Users\Jarvis\Documents\SoftServe\MaYBE with conflicts.

--- src/main/java/com/maybe/maybe/service/DeskService.java
+++ src/main/java/com/maybe/maybe/service/DeskService.java
@@ -1,19 +1,14 @@
 package com.maybe.maybe.service;
 
-
-import com.maybe.maybe.dto.DeskRequest;
+import com.maybe.maybe.dto.DeskDTO;
 import com.maybe.maybe.entity.Desk;
 import com.maybe.maybe.entity.enums.DeskState;
 import com.maybe.maybe.repository.DeskRepository;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Arrays;
 import java.util.List;
-
-import com.maybe.maybe.entity.Desk;
-import com.maybe.maybe.repository.DeskRepository;
-import org.springframework.stereotype.Service;
 
 @Service
 public class DeskService {
@@ -24,36 +19,38 @@ public class DeskService {
         this.deskRepository = deskRepository;
     }
 
-    public Desk getDeskById(Long id) {
+
+    public Desk findById(Long id) {
         if (deskRepository.findDeskById(id) != null) {
             return deskRepository.findDeskById(id);
+
         } else {
             throw new EntityNotFoundException("Can not find desk by id = " + id);
         }
     }
 
-    public List<Desk> getDeskList() {
-        if (!deskRepository.findAll().isEmpty()) {
-           return deskRepository.findAll();
+    public List<Desk> findAll() {
+        if (deskRepository.findDeskById(1L) != null) {
+            return deskRepository.findAll();
         } else {
             throw new EntityNotFoundException("Can not find any desk");
         }
     }
 
-    public Desk createDesk(DeskRequest deskRequest) {
-        if (deskRepository.findDeskByName(deskRequest.getName()) == null) {
+    public Desk createFromDTO(DeskDTO deskDTO) {
+        if (deskRepository.findDeskByName(deskDTO.getName()) == null) {
             Desk desk = new Desk();
-            desk.setName(deskRequest.getName());
+            desk.setName(deskDTO.getName());
             return deskRepository.save(desk);
         } else {
-            return null;
+            throw new BeanCreationException("Can not create desk entity by dto " + deskDTO.toString());
         }
     }
 
-    public Desk updateDeskById(Long id, DeskRequest deskRequest){
+    public Desk updateById(Long id, DeskDTO deskDTO){
         if(deskRepository.findDeskById(id) != null){
-            Desk desk = new Desk();
-            desk.setName(deskRequest.getName());
+            Desk desk = deskRepository.findDeskById(id);
+            desk.setName(deskDTO.getName());
             desk.setId(id);
             return deskRepository.save(desk);
         }else {
@@ -70,7 +67,13 @@ public class DeskService {
         }
     }
 
-    Desk getDesk(Long id){
-        return deskRepository.getOne(id);
+    public Desk deleteById(Long id){
+        if(deskRepository.findDeskById(id) != null){
+            Desk desk = deskRepository.findDeskById(id);
+            deskRepository.deleteById(id);
+            return desk;
+        } else {
+            throw new EntityNotFoundException("Can not delete desk by id = " + id);
+        }
     }
 }
